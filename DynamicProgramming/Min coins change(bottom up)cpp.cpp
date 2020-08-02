37e1c5d92a2d872a5a137a93111fb9c163c23ca3
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-08-02 10:10:48 +0530

    Add files via upload

---
+++ DynamicProgramming/Min coins change(bottom up)cpp.cpp
@@ -0,0 +1,58 @@
+// Top down
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fr(i,n)  for(int i=0;i<n;i++)
+#define fre(i,a,b)  for(int i=a;i<=b;i++)
+#define endl '\n'
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define mem(name, value) memset(name, value, sizeof(name))
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+#define case cout << "Case " << t++ << ": ";
+int Min_change(int *c, int n, int T)
+{
+	int dp[1000] = {0};
+	fr(int j = 1; i <= n; i++)
+	{
+		dp[j] = INT_MAX;
+		fr(i, T) {
+			if (n - c[i] >= 0) {
+				int sub = dp[n - c[i]];
+				dp[j] = min(dp[j], sub + 1)
+			}
+		}
+	}
+	return dp[n];
+}
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int T;
+		cin >> T;
+		int c[n];
+		fr(i, T) {
+			cin >> c[i];
+		}
+		int dp[] = {0};
+		cout << Min_change(c, n, T) << endl;
+	}
+	return 0;
+}
