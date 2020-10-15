115abf4a3e4b973e1e07e6fc2622cba8192b03b7
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-10-16 00:56:19 +0100

    Update Bishu and his Girlfriend.cpp

--- Hackerearth/Bishu and his Girlfriend.cpp
+++ Hackerearth/Bishu and his Girlfriend.cpp
@@ -15,66 +15,61 @@ using namespace std;
 #define case cout << "Case " << t++ << ": ";
 
 vector<int> adjlist[1001];
-map<int, int> dis;
+vector<int> vis;
+vector<int> dis;
 
-void bfs(int node, int d)
+void dfs(int f, int d)
 {
-	queue<int> q;
-	for (int i = 1; i <= 1001; i++)
-	{
-		dis[i] = INT_MAX;
-	}
-	q.push(node);
-	dis[node] = d;
-	while (!q.empty())
-	{
-		int f = q.front();
-		q.pop();
-		for (int it : adjlist[f])
-		{
-			if (dis[it] == INT_MAX)
-			{
-				q.push(it);
-				dis[it] = dis[f] + 1;
-			}
-		}
-	}
+    vis[f] = 1;
+    dis[f] = d;
+    for (int i : adjlist[f])
+    {
+        if (vis[i] == 0)
+        {
+            dfs(i, dis[f] + 1);
+        }
+    }
 }
 int32_t main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
-	int n, q;
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-	{
-		int u, v;
-		cin >> u >> v;
-		adjlist[u].pb(v);
-		adjlist[v].pb(u);
-	}
-	bfs(1, 0);
-	cin >> q;
-	int maxi = INT_MAX;
-	int ans = INT_MIN;
-	while (q--)
-	{
-		int g;
-		cin >> g;
-		if (dis[g] < maxi)
-		{
-			maxi = dis[g];
-			ans = g;
-		}
-		else if (dis[g] == maxi && g < ans)
-		{
-			ans = g;
-		}
-	}
-	cout << ans << endl;
+    int n, q;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adjlist[u].pb(v);
+        adjlist[v].pb(u);
+    }
+    for (int i = 1; i < 1001; i++)
+    {
+        vis.pb(0);
+        dis.pb(0);
+    }
+    dfs(1, 0);
+    cin >> q;
+    int maxi = INT_MAX;
+    int ans = -1;
+    while (q--)
+    {
+        int g;
+        cin >> g;
+        if (dis[g] < maxi)
+        {
+            maxi = dis[g];
+            ans = g;
+        }
+        else if (dis[g] == maxi && g < ans)
+        {
+            ans = g;
+        }
+    }
+    cout << ans << endl;
 }
