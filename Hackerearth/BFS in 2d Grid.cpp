087ed76bf96639da3c8b0d9c426d9d189e379a37
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-10-22 19:59:57 +0530

    Create BFS in 2d Grid.cpp

---
+++ Hackerearth/BFS in 2d Grid.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fr(i, a, n) for (int i = a; i < n; i++)
+#define fre(i, a, b) for (int i = a; i <= b; i++)
+#define endl '\n'
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define mem(name, value) memset(name, value, sizeof(name))
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+#define case cout << "Case " << t++ << ": ";
+
+int n,m;
+int arr[1001][1001];
+bool vis[1001][1001];
+int dis[1001][1001];
+
+bool valid(int x, int y)
+{
+	if(x<1 || x>m || y<1 || y>n)
+	{
+		return false;
+	}
+	else if(vis[x][y]==1)
+	{
+		return false;
+	}
+    else{
+		return true;
+	}
+}
+
+int dx[4]={-1,0,1,0};
+int dy[4]={0,1,0,-1};
+
+void BFS(int sx , int sy)
+{
+	queue<pair<int, int>> q;
+	q.push(mp(sx,sy));
+	vis[sx][sy] = 1;
+	dis[sx][sy] = 0;
+	while(!q.empty())
+	{
+		pair<int,int> p_curr = q.front();
+		cout<<p_curr.ff<<" "<<p_curr.ss<<endl;
+		q.pop();
+		for(int i=1;i<=4;i++)
+		{
+			if(valid(p_curr.ff+dx[i],p_curr.ss+dy[i]))
+			{
+				pair<int,int> p_new = mp(p_curr.ff+dx[i],p_curr.ss+dy[i]);
+				vis[p_new.ff][p_new.ss]=1;
+				dis[p_new.ff][p_new.ss] = dis[p_curr.ff][p_curr.ss] + 1;
+				q.push(p_new);
+			}
+		}
+
+	}
+} 
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	   cin>>n>>m;
+	   BFS(1,1);
+}
